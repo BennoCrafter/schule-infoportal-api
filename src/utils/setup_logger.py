a07fdf9bfd5e0b350cb169b95b3799978fc1b06e
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-10-27 19:56:35 +0100

    remove file handler

--- src/utils/setup_logger.py
+++ src/utils/setup_logger.py
@@ -5,8 +5,8 @@ def setup_logger(name: str):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
-    file_handler = logging.FileHandler("schule-infoportal-api.log")
-    file_handler.setLevel(logging.INFO)
+    # file_handler = logging.FileHandler("schule-infoportal-api.log")
+    # file_handler.setLevel(logging.INFO)
 
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
@@ -14,7 +14,7 @@ def setup_logger(name: str):
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
-    file_handler.setFormatter(formatter)
+    # file_handler.setFormatter(formatter)
     stream_handler.setFormatter(formatter)
 
     # logger.addHandler(file_handler)
