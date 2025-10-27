3c7f6438370e371cccc00aea72359dc45705ebaa
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-10-27 19:21:30 +0100

    Update setup_logger.py

--- src/utils/setup_logger.py
+++ src/utils/setup_logger.py
@@ -5,18 +5,19 @@ def setup_logger(name: str):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
-    file_handler = logging.FileHandler("lunchy_bot.log")
+    file_handler = logging.FileHandler("schule-infoportal-api.log")
     file_handler.setLevel(logging.INFO)
 
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
 
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     file_handler.setFormatter(formatter)
     stream_handler.setFormatter(formatter)
 
-    logger.addHandler(file_handler)
+    # logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
 
     logger.propagate = False
